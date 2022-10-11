174c5548cf06bc2b627797bd4c06a14447fc65c5
Tahmeedul Islam <70460495+thetahmeed@users.noreply.github.com>
2022-10-11 22:21:11 +0600

    Create 03_FixedPointIteration.c

---
+++ 03_FixedPointIteration.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <conio.h>
+#include <math.h>
+#include <stdlib.h>
+
+#define f(x) 3 * x - cos(x) + 1
+#define g(x) (cos(x) + 1) / 3
+
+int main()
+{
+    int step = 1, N;
+    float x0, x1, e;
+
+    printf("Enter initial guess: ");
+    scanf("%f", &x0);
+    printf("Enter tolerable error: ");
+    scanf("%f", &e);
+    printf("Enter maximum iteration: ");
+    scanf("%d", &N);
+
+    printf("\nStep\tx0\t\tf(x0)\t\tx1\t\tf(x1)\n");
+
+    do
+    {
+        x1 = g(x0);
+        printf("%d\t%f\t%f\t%f\t%f\n", step, x0, f(x0), x1, f(x1));
+
+        step = step + 1;
+
+        if (step > N)
+        {
+            printf("Not Convergent.");
+            exit(1);
+        }
+
+        x0 = x1;
+
+    } while (fabs(f(x1)) > e);
+
+    printf("\nRoot is %f", x1);
+
+    getch();
+    return (0);
+}
+
